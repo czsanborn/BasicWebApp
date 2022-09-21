ffebec24cf0c7950abf2c905091f22b04c0525a9
Carina Sanborn <carin@DESKTOP-MAG5CS8.localdomain>
2022-09-21 11:44:20 -0400

    Added new query

--- src/test/java/com/develogical/QueryProcessorTest.java
+++ src/test/java/com/develogical/QueryProcessorTest.java
@@ -30,4 +30,9 @@ public class QueryProcessorTest {
         assertThat(queryProcessor.process("what is your name"), containsString("MyTeam"));
     }
 
+    @Test
+    public void knowsHello() throws Exception {
+        assertThat(queryProcessor.process("Hello, my name is..."), containsString("Hi"));
+    }
+
 }
